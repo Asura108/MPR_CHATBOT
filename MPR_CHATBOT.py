29ad4439344a275ff34eb40e14bf37dcaaf70ca1
Deepanshu Molasi <85053641+Asura108@users.noreply.github.com>
2023-01-12 18:53:13 +0530

    Update MPR_CHATBOT.py

--- MPR_CHATBOT.py
+++ MPR_CHATBOT.py
@@ -3,7 +3,7 @@ import openai
 import streamlit as st
 from streamlit_chat import message
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 #creating a widget which can allow the users to change the temperature aspect of the model.
 temp = st.slider("Select temperature:", min_value=0.0, max_value=1.0, value=0.5, step=0.1)
